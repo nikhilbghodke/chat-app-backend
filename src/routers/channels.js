011d75ae102fb81cb54bf0d9ceeed0b2fb555f86
Nikhil Ghodke <nikhil.ghodke17@vit.edu>
2020-06-14 17:49:27 +0530

    channels routers modified

--- src/routers/channels.js
+++ src/routers/channels.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const auth = require("../middlewares/roomOwnerAuth")
+const roomOwner= require("../middlewares/roomOwnerAuth.js")
+const roomMember= require("../middlewares/roomMember.js")
 const Room = require("../models/room.js")
 const User = require("../models/user.js")
 const Channel = require("../models/channel")
@@ -7,14 +9,29 @@ const app = express.Router({ mergeParams: true });
 
 
 //create channel sending room title to create 
-app.post("/channels/:title", auth, async (req, res, next) => {
+app.post("/channels/:title", roomOwner, async (req, res, next) => {
     try {
+        var ifexists=await Channel.findOne({
+            title:req.body.title,
+            room:req.room._id
+        })
+        console.log(!ifexists)
+        if(ifexists)
+        {
+            return next({
+                status:400,
+                message:"Already a channel with "+req.body.title+" exists in this room"
+            })
+        }
+        
         var channel = new Channel(req.body)
         channel.room = req.room.id
         await channel.save()
+        console.log(channel)
         res.status(201).send(channel)
     }
     catch (e) {
+        console.log(e)
         return next({
             message: e.message
         })
@@ -22,10 +39,12 @@ app.post("/channels/:title", auth, async (req, res, next) => {
 })
 
 //get single channel by title
-app.get("/channels/:title", async (req, res, next) => {
+app.get("/room/:title/channels/:channel",roomMember ,async (req, res, next) => {
     try {
+        console.log()
         var channel = await Channel.findOne({
-            title: req.params.title
+            title: req.params.channel,
+            room:req.room._id
         })
         if (!channel)
             return next({
@@ -41,7 +60,7 @@ app.get("/channels/:title", async (req, res, next) => {
 })
 
 //delete channel room title and channel name in path to del
-app.delete("/channels/:title/:name", auth, async (req, res, next) => {
+app.delete("/room/:title/channels/:name", roomOwner, async (req, res, next) => {
     try {
         var channel = await Channel.findOne({
             title: req.params.name
@@ -79,10 +98,11 @@ app.get("/allChannels", async (req, res, next) => {
 })
 
 //update channel
-app.patch("/channels/:title/:name", auth, async (req, res, next) => {
+app.patch("/room/:title/channels/:name", roomOwner, async (req, res, next) => {
     try {
         var channel = await Channel.findOne({
-            title: req.params.name
+            title: req.params.name,
+            room:req.room._id
         })
         if (!channel)
             return res.status(404).send("No such channel available")
@@ -101,10 +121,10 @@ app.patch("/channels/:title/:name", auth, async (req, res, next) => {
 })
 
 //get all channels in room
-app.get("/allChannels/:title", async (req, res, next) => {
-    var roomid = req.params.roomid
+app.get("/room/:title/allChannels", async (req, res, next) => {
+    var roomid = req.params.title
     try {
-        const romm = await Room.findOne({
+        const room = await Room.findOne({
             title:req.params.title
         })
         const channels = await Channel.find({
